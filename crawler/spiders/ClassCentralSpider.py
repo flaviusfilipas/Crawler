8f1e2a65c091f0923b36b5e2ac3021de2cd20b94
Flavius Filipas <flaviuslivada@gmail.com>
2019-04-09 22:05:26 +0300

    added ClassCentral spider

---
+++ crawler/spiders/ClassCentralSpider.py
@@ -0,0 +1,33 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "crawler"
+    start_urls = [
+        'https://www.classcentral.com/search?q=photography'
+    ]
+
+    def parse(self, response):
+        base_url = 'https://www.classcentral.com'
+        links = response.css('td.course-name-column a.course-name::attr(href)').getall()
+        for link in links:
+            if link[0] is not '/':
+                print(link)
+                continue
+            else:
+                return scrapy.Request(base_url + link, callback=self.parse_course_info)
+
+    def parse_course_info(self, response):
+        item = {
+            'title': response.css('h1#course-title::text').get(),
+            'provider': response.css('a.text-2::text').get(),
+            'link_to_course': response.css('a#btnProviderCoursePage::attr(href)').get()
+        }
+        yield item
+
+    # def parse_info(self, response):
+    #     item = {
+    #         'title': response.css('h1#course-title::text').extract(),
+    #         'provider': response.css('a.text--charcoal::text').extract()
+    #     }
+    #     return item
